2703b171d5cb0c986ffbea2570917dfc808da092
Myron <josephonyekachi77@gmail.com>
2023-06-03 02:20:05 -0800

    updated now

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className='flex sm:justify-between sm:items-center sm:bg-slate-200 sm:p-3 sm:shadow-sm flex-col sm:flex-row'>
+      <div className="menu">
+        <ul className='flex justify-evenly items-center sm:w-[200px] p-3 sm:p-0 bg-slate-200'>
+          <li><Link href="/" className='hover:text-yellow-500 font-semibold'>Home</Link></li>
+          <li><Link href="/about" className='hover:text-yellow-500 font-semibold'>About</Link></li>
+          <li><Link href="/" className='hover:text-yellow-500 font-semibold'>Home</Link></li>
+        </ul>
+      </div>
+
+      <div className='flex p-3 sm:p-0'>
+        <input type="text" className='p-1 rounded w-[100%] border' placeholder='Search...' />
+
+        <button className='bg-blue-600 text-white rounded px-2 ml-2'>Search</button>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      screens: {
+        'smx': {'max': '100px'},
+        // => @media (max-width: 639px) { ... }
+      }
     },
   },
   plugins: [],
