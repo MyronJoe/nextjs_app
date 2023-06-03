a5f51af15f4317efbb6a32fb07c442088f0ce800
Myron <josephonyekachi77@gmail.com>
2023-06-03 02:20:05 -0800

    updated now

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
     return (
         <div>
 
-            <h1>AboutPage</h1>
+            <h1>About My Tech Life</h1>
 
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia repudiandae modi repellat facere sequi numquam voluptatem quam, vel eius minus iste accusantium itaque exercitationem reprehenderit quasi rem porro hic eum nam vero. Ducimus explicabo atque vel, possimus repellat consequatur dolorem culpa veniam assumenda voluptatum nemo, eius, velit neque commodi quasi.</p>
 
---
+++ app/about/team/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+        <h1>Team</h1>
+
+        <ul>
+            <li>Myron Joe</li>
+            <li>Okey Mike</li>
+            <li>Harry Are</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Team
