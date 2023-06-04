eaaf07e09308d9b418fb96a2e590218809a5c7cb
Myron <josephonyekachi77@gmail.com>
2023-06-04 02:20:05 -0800

    updated now

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ weight:['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Myron Joe NextJS App',
