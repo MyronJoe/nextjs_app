495d7dae4d419d9895cfe098623be667fa4797fa
Myron <josephonyekachi77@gmail.com>
2023-06-04 02:20:05 -0800

    updated now

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <div className='flex sm:justify-between sm:items-center sm:bg-slate-200 sm:p-3 sm:shadow-sm flex-col sm:flex-row'>
+            <div className="menu">
+                <ul className='flex justify-evenly items-center sm:w-[200px] p-3 sm:p-0 bg-slate-200'>
+                    <li><Link href="/" className='links'>Home</Link></li>
+                    <li><Link href="/about" className='links'>About</Link></li>
+                    <li><Link href="/about/team" className='links'>Team</Link></li>
+                </ul>
+            </div>
+
+            <div className='flex p-3 sm:p-0'>
+                <input type="text" className='p-1 rounded w-[100%] border outline-none' placeholder='Search...' />
+
+                <button className='bg-blue-600 text-white rounded px-2 ml-2 hover:bg-blue-400'>Search</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
 
-const inter = Inter({ weight:['400', '700'], subsets: ['latin'] })
+const inter = Inter({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Myron Joe NextJS App',
@@ -12,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='container mx-auto'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
